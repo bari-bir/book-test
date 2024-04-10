0d98a2c6435284695ab780c257d86bd6d303951c
yrysNM <yrys.n3m@gmail.com>
2024-04-10 16:06:49 +0500

    init project

---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  singleQuote: false,
+  trailingComma: "all",
+  tabWidth: 4,
+  printWidth: 150,
+  semi: false,
+  bracketSameLine: true,
+};
---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import "./assets/styles/global.scss"
+function App() {
+    return <div></div>
+}
+
+export default App
